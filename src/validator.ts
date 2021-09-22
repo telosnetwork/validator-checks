925785bba4f1ae83dd6006adcb147ce503968769
DonaldPeat <don@caleos.io>
2021-09-22 16:09:02 -0500

    feat: add direct axios methods

--- src/validator.ts
+++ src/validator.ts
@@ -1,7 +1,10 @@
 import { BlockProducerHttpClient } from "./types/BlockProducerHttpClient";
 import { BlockProducer } from "./types/BlockProducer";
+import axios from 'axios';
 
-export async function getProducersArray(urlArray: string[]): Promise<BlockProducer[]>{
+
+//to use ininterceptors 
+export async function getProducersArrayUsingClient(urlArray: string[]): Promise<BlockProducer[]>{
     const producerInfoArray: BlockProducer[] = [];
     for (const url of urlArray){
         const producerClient = new BlockProducerHttpClient(url);
@@ -12,8 +15,26 @@ export async function getProducersArray(urlArray: string[]): Promise<BlockProduc
     return producerInfoArray;
 }
 
+const bpPath = '/bp.json';
+const chainPath = 'chains.json';
+
+export async function getProducersArray(urlArray: string[]): Promise<BlockProducer[]>{
+    const producerInfoArray: BlockProducer[] = [];
+    for (const url of urlArray){
+        try{
+            const rawData = await axios.get(`${url}${bpPath}`);
+            rawData.data.chains = await axios.get(`${url}${chainPath}`);
+            const producer = new BlockProducer(rawData.data);
+            producerInfoArray.push(producer);
+        }catch(error){
+            console.error(error);
+        }
+    }
+    return producerInfoArray;
+}
+
 (async () => {
-    const producerUrlArray:string[] = ["https://goodblock.io/", "https://caleos.io"];
-    const test = await getProducersArray(producerUrlArray);
+    const producerUrlArray:string[] = ["https://goodblock.io/", "https://caleos.io"]
+    const test = await getProducersArrayUsingClient(producerUrlArray);
     console.dir(test);
 })()
