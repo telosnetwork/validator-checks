d8c6da386e9591ab5cfb3af18af891759ffe83d2
DonaldPeat <don@caleos.io>
2021-10-23 00:08:55 -0500

    fix: add remainder method, update array assignment

--- src/main.ts
+++ src/main.ts
@@ -13,31 +13,19 @@ export async function getProducer(owner: string): Promise<BlockProducer[]>{
 export async function getProducers(limit: number): Promise<BlockProducer[]>{
     const count = Math.floor( limit / FETCH_COUNT)
     const remainder = limit % FETCH_COUNT;
-    const producerData = [] as BlockProducer[];
-
+    let producerData = [] as BlockProducer[];
     let i = 1;
     let next = '';
 
     while(i < count + 1){
-        const producerArray =  await getProducerData(10, next);
-        producerData.concat(producerArray.data);
+        const producerArray =  await getProducerData(FETCH_COUNT, next);
+        producerData = [ ...producerData, ...producerArray.data];
         next = producerArray.key;
         i++
     }
     if (remainder){
         const producerArray = await getProducerData(remainder, next);
-        producerData.concat(producerArray.data);
+        producerData = [ ...producerData, ...producerArray.data];
     }
     return producerData;
 }
-
-
-(async () => {
-    const producer = await getProducer('caleosblocks');
-    console.dir(producer);
-
-    const producers = await getProducers(6);
-    console.dir(producers);
-
-
-})()
